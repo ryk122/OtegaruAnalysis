7f17d45ced8a1140350c24dcd73eb3afaea31378
ryk122 <54937624+ryk122@users.noreply.github.com>
2020-12-16 20:50:01 +0900

    json file reader

--- OtegaruAnalysis/Form1.Designer.cs
+++ OtegaruAnalysis/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // label1
@@ -45,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(510, 37);
+            this.button1.Location = new System.Drawing.Point(550, 177);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 55);
             this.button1.TabIndex = 1;
@@ -62,11 +64,26 @@
             this.textBox1.Size = new System.Drawing.Size(369, 241);
             this.textBox1.TabIndex = 2;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(550, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Open Json..";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "\"JSON (*.json)|*.json|すべてのファイル (*.*)|*.*\"";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -82,6 +99,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- OtegaruAnalysis/Form1.cs
+++ OtegaruAnalysis/Form1.cs
@@ -9,26 +9,49 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Text.Json;
+using System.Globalization;
 
 
 namespace OtegaruAnalysis
 {
     public partial class Form1 : Form
     {
+        private string jsonfile;
+
         public Form1()
         {
             InitializeComponent();
+            label1.Text = "open json file!";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "hello";
             LoadJson();
+            label1.Text = GetUnixTime(DateTime.Now).ToString();
+        }
+
+        private void OpenFile()
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+                jsonfile = openFileDialog1.FileName;
+            }
         }
 
         private void LoadJson()
         {
-            string json = "[{\"os\":\"android\",\"coin\":230,\"created_at\":1607746294014},{\"coin\":1050,\"created_at\":1608013732727,\"os\":\"android\"}]";
+            //string json = "[{\"os\":\"android\",\"coin\":230,\"created_at\":1607746294014},{\"coin\":1050,\"created_at\":1608013732727,\"os\":\"android\"}]";
+
+            if (jsonfile==null)
+            {
+                OpenFile();
+                return;
+            }
+            //string FilePath = @"C:\Users\ryk\Desktop\get_active_logs.json"; //ファイルパス
+            string FilePath = jsonfile; //ファイルパス
+            var json = File.ReadAllText(FilePath); // ファイル内容をjson変数に格納
             List<OtegaruData> data = JsonSerializer.Deserialize<List<OtegaruData>>(json);
 
             textBox1.Text += data.Count().ToString();
@@ -38,6 +61,26 @@ namespace OtegaruAnalysis
         {
             File.WriteAllText(@"C:\Users\ryk\Desktop\test.txt", "Good morning!");
         }
+
+        // UNIXエポックを表すDateTimeオブジェクトを取得
+        private static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        public static long GetUnixTime(DateTime targetTime)
+        {
+            // UTC時間に変換
+            targetTime = targetTime.ToUniversalTime();
+
+            // UNIXエポックからの経過時間を取得
+            TimeSpan elapsedTime = targetTime - UNIX_EPOCH;
+
+            // 経過秒数に変換
+            return (long)elapsedTime.TotalSeconds;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
     }
 
     public class OtegaruData
@@ -46,4 +89,6 @@ namespace OtegaruAnalysis
         public float created_at { get; set; }
         public string os { get; set; }
     }
+
+
 }
